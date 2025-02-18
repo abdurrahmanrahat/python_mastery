f24633c2a45fddaeef59b8680805b4e5f7c4870c
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-02-18 19:40:40 +0600

    string

---
+++ chai_aur_code/03.string/string.py
@@ -0,0 +1,14 @@
+name = "Abdur Rahman"
+print(name)
+
+first_char = name[0]
+print(first_char)
+
+slice_name = name[0:3]
+print(slice_name)
+
+last_char = name[-1]
+print(last_char)
+
+print(name.find("Rahman")) # 6
+
