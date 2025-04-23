fd30627da138dc1122cb4914a6c5345819b6445b
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-04-23 09:52:06 +0600

    prob solve 3 and 4

---
+++ chai_aur_code/07.confitionals_prob/03_solution.py
@@ -0,0 +1,14 @@
+mark = 66
+
+if mark > 100:
+    print("wrong mark assign!")
+elif mark >= 90:
+    print("a")
+elif mark >= 80:
+    print("b")
+elif mark >= 70:
+    print("c")
+elif mark >= 60:
+    print("d")
+else:
+    print("f")
---
+++ chai_aur_code/07.confitionals_prob/04_solution.py
@@ -0,0 +1,11 @@
+fruit = "banana"
+color = "green"
+
+if color == "green":
+    print("Unripe")
+elif color == "Yellow":
+    print("Ripe")
+elif color == "Brown":
+    print("Overripe")
+else: 
+    print("unknown color!")
