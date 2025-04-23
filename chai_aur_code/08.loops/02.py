8dc08fe2011d9f1b99e84a2379d438b2c1e66954
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-04-23 19:04:04 +0600

    prob solve 3 and 4

---
+++ chai_aur_code/08.loops/02.py
@@ -0,0 +1,9 @@
+number = 3
+
+sumOfNumbers = 0
+
+for i in range(1, number + 1):
+    if i % 2 == 0:
+        sumOfNumbers += i
+
+print("sumOfNumbers", sumOfNumbers)
---
+++ chai_aur_code/08.loops/03.py
@@ -0,0 +1,6 @@
+number = 6
+
+for i in range(1, 11):
+    if i == 5:
+        continue
+    print(i, "*", number, "=", i * number)
