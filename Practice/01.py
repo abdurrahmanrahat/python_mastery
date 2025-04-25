2cb14d36ce0510bd1f23013531cae08df5a38408
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-04-25 22:09:58 +0600

    problem solve in mutuable and immutable

---
+++ Practice/01.py
@@ -0,0 +1,16 @@
+# Write a Python script that:
+
+# Takes two integers as input from the user.
+# Prints their sum, product, and integer division result.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sumOfTwoNumbers = num1 + num2
+print("sumOfTwoNumbers", sumOfTwoNumbers)
+
+multifyOfTwoNumbers = num1 * num2
+print("multifyOfTwoNumbers", multifyOfTwoNumbers)
+
+divisionOfTwoNumbers = num1 // num2
+print("divisionOfTwoNumbers", divisionOfTwoNumbers)
