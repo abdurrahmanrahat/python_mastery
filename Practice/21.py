202525658916d31f8ae64c47a2698ab074b62800
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-04-27 19:25:05 +0600

    test code

---
+++ Practice/21.py
@@ -0,0 +1,22 @@
+fruit_colors = {
+    "apple": "red",
+    "banana": "yellow",
+    "grape": "purple"
+}
+
+# Accessing data
+print(fruit_colors["apple"])   # Output: red
+print(fruit_colors["banana"])  # Output: yellow
+
+print(2 * 3 + 4 / 2)
+
+string = "22"
+
+print(int(string))
+
+for i in range(1, 6):
+    print(i)
+
+
+# if condition:
+#     # code block to execute if condition is True
---
+++ Practice/22.py
@@ -0,0 +1,11 @@
+numbers = [1, 2, 3, 4]
+
+total = 0
+
+for num in numbers:
+	total += num
+ 
+if total <= 10:
+	total += 1
+
+print(total)
